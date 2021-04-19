d39e7a830ba3c7d9ca2b4a52f6ec72e9671f30ea
miketwheeler <mikeytwheeler@gmail.com>
2021-04-19 13:25:16 -0700

    sign in page; firebase added

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_elevated_button.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent() {
+    //private returned widget
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 48.0), // seperator
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0), // seperator
+          SocialSignInButton(
+            assetName: 'images/facebook-logo.png',
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0), // seperator
+          SignInButton(
+            text: 'Sign in with Email',
+            textColor: Colors.white,
+            color: Color(0xFF00796B),
+            onPressed: () {},
+          ),
+          SizedBox(height: 12.0), // seperator
+          Text(
+            'or',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w400,
+              color: Colors.black87,
+            ),
+          ),
+          SizedBox(height: 12.0), // seperator
+          SignInButton(
+            text: 'Go Anonymous',
+            textColor: Colors.black,
+            color: Color(0xFFDCE775),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
